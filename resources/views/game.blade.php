0eea64b249688ac17b2ac3160926751a7cf4cf45
Didier Berlioz <berliozd@gmail.com>
2025-01-08 22:58:05 +0100

    Update code boxes style

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -16,6 +16,9 @@
             .share-buttons {
                 display: none; /* Hidden by default */
             }
+            code[class*=language-], pre[class*=language-] {
+                font-size: small;
+            }
         </style>
 
         <div class="share-buttons flex flex-col gap-2 md:flex-row justify-between">
@@ -69,23 +72,26 @@
         {{$game->description}}
     </div>
 
-    <div class="w-3/4 mx-auto my-20 flex flex-col md:flex-row gap-2">
-        <div class="overflow-hidden">
-            <div>CSS</div>
-            <div class="mockup-code">
-                <pre class="text-xs text-wrap p-2 h-96"><code class="language-css">{{ $game->css }}</code></pre>
+    <div class="w-3/4 mx-auto mb-20 mt-8">
+        <div class="underline text-2xl mb-2">Here is the code</div>
+        <div class="flex flex-col md:flex-row gap-2">
+            <div class="overflow-hidden">
+                <div>CSS</div>
+                <div class="mockup-code">
+                    <pre class="h-96"><code class="language-css">{{$game->css}}</code></pre>
+                </div>
             </div>
-        </div>
-        <div class="overflow-hidden">
-            <div class="text-wrap">HTML</div>
-            <div class="mockup-code">
-                <pre class="text-xs text-wrap p-2 h-96"><code class="language-html">{{ $game->html }}</code></pre>
+            <div class="overflow-hidden">
+                <div class="text-wrap">HTML</div>
+                <div class="mockup-code">
+                    <pre class="text-wrap p-2 h-96"><code class="language-html">{{ $game->html }}</code></pre>
+                </div>
             </div>
-        </div>
-        <div class="overflow-hidden">
-            <div>JS</div>
-            <div class="mockup-code">
-                <pre class="text-xs text-wrap p-2 h-96"><code class="language-js">{{ $game->js }}</code></pre>
+            <div class="overflow-hidden">
+                <div>JS</div>
+                <div class="mockup-code">
+                    <pre class="text-wrap p-2 h-96"><code class="language-js">{{ $game->js }}</code></pre>
+                </div>
             </div>
         </div>
     </div>
